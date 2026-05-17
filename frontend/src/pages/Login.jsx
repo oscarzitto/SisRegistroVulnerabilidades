92033a2e0a10a25766e286cd2b825c582df571a1
oscarzitto <oscar.rodrigo2001@hotmail.com>
2026-05-17 19:03:43 -0400

    login si funciona

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,54 @@
+import {useState} from "react";
+import api from "../services/api";
+
+function Login(){
+
+const[correo,setCorreo]=useState("");
+const[password,setPassword]=useState("");
+
+const enviar=async()=>{
+
+const res=await api.post(
+"/login",
+{
+correo,
+password
+});
+
+localStorage.setItem(
+"token",
+res.data.token
+);
+
+alert("Login correcto");
+
+};
+
+return(
+
+<div>
+
+<h1>Login</h1>
+
+<input
+placeholder="correo"
+onChange={(e)=>setCorreo(e.target.value)}
+/>
+
+<input
+type="password"
+placeholder="contraseña"
+onChange={(e)=>setPassword(e.target.value)}
+/>
+
+<button onClick={enviar}>
+Entrar
+</button>
+
+</div>
+
+);
+
+}
+
+export default Login;
