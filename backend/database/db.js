ec45c72f096d0c883ab6270bfae0b5143fb26661
oscarzitto <oscar.rodrigo2001@hotmail.com>
2026-05-17 16:23:12 -0400

    prueva

---
+++ backend/database/db.js
@@ -0,0 +1,37 @@
+const sqlite3 = require("sqlite3").verbose();
+
+const db = new sqlite3.Database("./database/database.db", (err) => {
+    if (err) {
+        console.log("Error BD:", err.message);
+    } else {
+        console.log("Base de datos conectada");
+
+        db.run(`
+        CREATE TABLE IF NOT EXISTS usuarios(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nombre TEXT NOT NULL,
+            correo TEXT UNIQUE NOT NULL,
+            password_hash TEXT NOT NULL,
+            rol TEXT NOT NULL
+        )
+        `);
+
+        db.run(`
+        CREATE TABLE IF NOT EXISTS hallazgos(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            fecha TEXT,
+            activo_afectado TEXT,
+            tipo TEXT,
+            severidad TEXT,
+            descripcion TEXT,
+            evidencia TEXT,
+            recomendacion TEXT,
+            estado TEXT,
+            responsable TEXT
+        )
+        `);
+
+    }
+});
+
+module.exports = db;
